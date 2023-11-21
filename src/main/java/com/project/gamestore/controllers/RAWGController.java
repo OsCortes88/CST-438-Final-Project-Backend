42213cc52f7f2e0b5e442f710b1e059ca0ead884
Carlos <cgsantiagop@gmail.com>
2023-11-20 22:34:25 -0800

    I'm making this commit to save the changes done to RAWGController Making sure it works now

--- src/main/java/com/project/gamestore/controllers/RAWGController.java
+++ src/main/java/com/project/gamestore/controllers/RAWGController.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -36,6 +37,27 @@ public class RAWGController {
         JSONArray gamesList = json.getJSONArray("results");
         // Make the list of video games from the json list we retrieved from RAWG
         List<VideoGame> games = new ObjectMapper().readValue(gamesList.toString(), new TypeReference<List<VideoGame>>() {});
+        return games;
+    }
+
+
+    @GetMapping("/videogames/genre")
+    public List<VideoGame> listGamesByGenre(
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "") String sortBy,
+            @RequestParam String genres
+    ) throws JsonProcessingException {
+        // Call the RAWG API to get a certain number of games for a specific genre
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                "https://api.rawg.io/api/games?page_size=" + pageSize + "&genres=" + genres + "&key=79fc5d7fcd144b99ade6f0aafc6e8c74",
+                String.class);
+        String jsonString = response.getBody();
+
+        JSONObject json = new JSONObject(jsonString);
+        JSONArray gamesList = json.getJSONArray("results");
+
+        List<VideoGame> games = new ObjectMapper().readValue(gamesList.toString(), new TypeReference<List<VideoGame>>() {});
+
         return games;
     }
 }
