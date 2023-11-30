1e54082d42c51c63164fad47186d0757877b6648
f-pulido <fpulido@csumb.edu>
2023-11-30 11:43:09 -0800

    Added a fix to the search method where it now displays new video game information.

--- src/main/java/com/project/gamestore/controllers/RAWGController.java
+++ src/main/java/com/project/gamestore/controllers/RAWGController.java
@@ -62,20 +62,41 @@ public class RAWGController {
 
     @GetMapping("/videogames/search")
     public List<VideoGame> ListGamesByName(@RequestParam String title) throws JsonProcessingException {
-        // Process title
+        // Process title.
         processTitle(title);
 
-        // Call the RAWG API to get a certain number of games
+        String url = "https://api.rawg.io/api/games?search=" + title + "&page_size=10&key=79fc5d7fcd144b99ade6f0aafc6e8c74";
+
+        // Call the RAWG API to get a certain number of games.
         ResponseEntity<String> response = restTemplate.getForEntity(
-                "https://api.rawg.io/api/games?search=" + title + "&page_size=10&key=79fc5d7fcd144b99ade6f0aafc6e8c74",
+                url,
                 String.class);
         String jsonString = response.getBody();
 
-        // Convert the json string to a json object, so we can access data.
-        JSONObject json = new JSONObject(jsonString);
-        JSONArray gamesList = json.getJSONArray("results");
-  
-        List<VideoGame> games = new ObjectMapper().readValue(gamesList.toString(), new TypeReference<List<VideoGame>>() {});
+        // Convert the json string to a json node, so we can access data.
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readValue(jsonString, JsonNode.class);
+
+        // Move into the array with the list of games.
+        JsonNode gamesList = rootNode.get("results");
+
+        // Iterate through the games and retrieve the game info.
+        List<VideoGame> games = new ArrayList<>();
+        for (int i = 0; i < gamesList.size(); i++) {
+            VideoGame g = new VideoGame();
+            g.setId(Integer.parseInt(String.valueOf(gamesList.get(i).get("id"))));
+            g.setName(String.valueOf(gamesList.get(i).get("name")));
+            g.setDescription(String.valueOf(gamesList.get(i).get("description")));
+            g.setBackgroundImage(String.valueOf(gamesList.get(i).get("background_image")));
+            g.setReleased(String.valueOf(gamesList.get(i).get("released")));
+            g.setEsrb(String.valueOf(gamesList.get(i).get("esrb_rating")));
+            g.setRating(Double.parseDouble(String.valueOf(gamesList.get(i).get("rating"))));
+            g.setPlaytime(Integer.parseInt(String.valueOf(gamesList.get(i).get("playtime"))));
+            g.setScreenshots(getGameScreenShots(g.getId()));
+            g.setTrailers(getGameTrailers(g.getId()));
+            g.setPurchaseSites(getVendors(g.getId()));
+            games.add(g);
+        }
         return games;
     }
 
