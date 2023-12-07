85554bb6729baf581dc6c2c071c9fbdc1aab9dd0
OsCortes88 <ocortestinoco@csumb.edu>
2023-12-07 04:12:35 -0800

    add principal to methods

--- src/main/java/com/project/gamestore/controllers/WishListController.java
+++ src/main/java/com/project/gamestore/controllers/WishListController.java
@@ -7,42 +7,46 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
 public class WishListController {
     private RestTemplate restTemplate = new RestTemplate();
     @Autowired
+    UserRepository userRepository;
+    @Autowired
     WishListRepository wishListRepository;
     @Autowired
     VideoGameRepository videoGameRepository;
 
-    @PutMapping("/add-game/{userID}/{gameID}")
-    public boolean addGameToWishList(@PathVariable("userID") Integer userId,
-                                       @PathVariable("gameID") Integer gameID) throws Exception {
+    @PutMapping("/add-game/{gameID}")
+    public boolean addGameToWishList(Principal principal,
+                                     @RequestBody VideoGameDTO gameInfo) throws Exception {
         // Get the video game data from the RAWGController
-        if(wishListRepository.findByGameId(gameID) == null) {
-            VideoGameDTO response = restTemplate.getForObject(
-                    "http://localhost:8080/videogame-info/" + gameID,
-                        VideoGameDTO.class);
+        User user = userRepository.findByEmail(principal.getName());
+        if(wishListRepository.findByGameId((int) user.getId(), gameInfo.id()) == null) {
+//            VideoGameDTO response = restTemplate.getForObject(
+//                    "http://localhost:8080/videogame-info/" + gameID,
+//                        VideoGameDTO.class);
             // Set video game info and store it in videogame table
             VideoGame game = new VideoGame();
-            game.setId(response.id());
-            game.setName(response.name());
-            game.setDescription(response.description());
-            game.setReleased(response.released());
-            game.setBackgroundImage(response.background_image());
-            game.setRating(response.rating());
-            game.setPlaytime(response.playtime());
-            game.setEsrb(response.esrb_rating());
+            game.setId(gameInfo.id());
+            game.setName(gameInfo.name());
+            game.setDescription(gameInfo.description());
+            game.setReleased(gameInfo.released());
+            game.setBackgroundImage(gameInfo.background_image());
+            game.setRating(gameInfo.rating());
+            game.setPlaytime(gameInfo.playtime());
+            game.setEsrb(gameInfo.esrb_rating());
             videoGameRepository.save(game);
 
             // Create the wishlist object that will later be used in the repository
             WishListItem wishlistItem = new WishListItem();
-            wishlistItem.setUserId(userId);
-            wishlistItem.setGameId(response.id());
-            wishlistItem.setName(response.name());
-            wishlistItem.setBackground_image(response.background_image());
+            wishlistItem.setUserId((int) user.getId());
+            wishlistItem.setGameId(gameInfo.id());
+            wishlistItem.setName(gameInfo.name());
+            wishlistItem.setBackground_image(gameInfo.background_image());
             wishListRepository.save(wishlistItem);
             return true;
         } else {
@@ -50,20 +54,25 @@ public class WishListController {
         }
     }
 
-    @DeleteMapping("/delete-game/{userId}/{gameId}")
-    public boolean deleteGameFromWishlist(@PathVariable("userId") Integer userId,
+    @DeleteMapping("/delete-game/{gameId}")
+    public boolean deleteGameFromWishlist(Principal principal,
                                        @PathVariable("gameId") Integer gameId) throws Exception {
-        if (wishListRepository.findByGameId(gameId) != null) {
-            wishListRepository.deleteByGameId(gameId);
+        User user = userRepository.findByEmail(principal.getName());
+        if (wishListRepository.findByGameId((int) user.getId(), gameId) != null) {
+            wishListRepository.deleteByGameId((int) user.getId(), gameId);
             return true;
         } else {
             throw new Exception("wishlist item not found");
         }
     }
 
-    @GetMapping("/wishlist/{userId}")
-    public List<WishListItem> getItemsInUserWishList(@PathVariable("userId") Integer userId) {
-        List<WishListItem> userWishList = wishListRepository.findUsersWishList(userId);
+    @GetMapping("/wishlist")
+    public List<WishListItem> getItemsInUserWishList(Principal principal) {
+        User userInfo = userRepository.findByEmail(principal.getName());
+        System.out.println(userInfo);
+        System.out.println("" + wishListRepository.findAll());
+        List<WishListItem> userWishList = wishListRepository.findUsersWishList((int) userInfo.getId());
+        System.out.println("User wishList: " + userWishList.get(0).getBackground_image());
         return userWishList;
     }
 
